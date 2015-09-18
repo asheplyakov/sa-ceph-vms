5b2aca12483d846cf1c9b045b858e92bca42ab9d
Alexei Sheplyakov <asheplyakov@mirantis.com>
2015-09-18 12:10:44 +0300

    Ceph virtual lab configuration -- initial version

---
+++ rebuild-vm.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+set -e
+MYSELF="${0##*/}"
+
+vm="$1"
+
+if [ -z "$1" ]; then
+	echo "$MYSELF: Error: no VM has been specified" >&2
+	exit 1
+fi
+
+UBUNTU_IMG="/srv/data/Public/img/trusty-server-cloudimg-amd64-disk1.img"
+UBUNTU_IMG_URL="https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img"
+
+if [ ! -f "$UBUNTU_IMG" ]; then
+	wget -N -O "$UBUNTU_IMG" "$UBUNTU_IMG_URL"
+fi
+
+
+cd "${0%/*}"
+virsh destroy "$vm" || true
+if ! virsh domid "$vm" >/dev/null 2>&1; then
+	virsh define "${vm}.xml"
+fi
+rm -f "${vm}.qcow2"
+cp -a $UBUNTU_IMG "${vm}.qcow2"
+./mkconfdrive "$vm"
+exec virsh start "$vm"

974f645dfd11c90163dab1f569fa320eb01cea4b
Alexey Sheplyakov <asheplyakov@mirantis.com>
2015-12-25 20:22:17 +0300

    deploy-ceph.sh: erase old keys and data (if any)
    Now it's possible to redeploy ceph without rebuilding VMs.

--- deploy-ceph.sh
+++ deploy-ceph.sh
@@ -2,15 +2,23 @@
 set -e
 ADM="saceph-adm"
 MON="saceph-mon"
+RGW="saceph-rgw"
 OSDS="saceph-osd1 saceph-osd2 saceph-osd3"
 
 mkdir -p -m 0755 ~/.ssh
-for node in $MON $OSDS; do
+for node in $MON $OSDS $ADM $RGW; do
+	ssh-keygen -f ~/.ssh/known_hosts -R "$node"
+done
+for node in $MON $OSDS $ADM $RGW; do
 	ssh-keyscan -t rsa $node
 done >> ~/.ssh/known_hosts
 
+ceph-deploy --overwrite-conf purge $MON $OSDS $RGW $ADM
+ceph-deploy --overwrite-conf purgedata  $MON $OSDS $RGW $ADM
+ceph-deploy --overwrite-conf forgetkeys
+
 ceph-deploy --overwrite-conf new $MON
-ceph-deploy --overwrite-conf install $MON $OSDS
+ceph-deploy --overwrite-conf install $MON $OSDS $ADM $RGW
 ceph-deploy --overwrite-conf mon create-initial
 for osd in $OSDS; do
 	ceph-deploy --overwrite-conf disk zap ${osd}:vdb

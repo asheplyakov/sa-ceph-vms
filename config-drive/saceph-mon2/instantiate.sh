404ce53e5c82a9f5d0e0315d68e2e0241e01c0be
Alexey Sheplyakov <asheplyakov@mirantis.com>
2016-04-19 12:58:19 +0300

    Add 4GB swap partition to all VMs
    4GB of RAM might be not quite enough for running 2 OSDs

---
+++ config-drive/saceph-mon2/instantiate.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+#
+# IPs:
+# mons: [10.253.0.20, 10.253.0.100)
+# OSDs: [10.253.0.100, 10.253.0.248)
+# rgws: [10.253.0.248, 10.253.0.254]
+
+MY_DIR="${0%/*}"
+
+cd "$MY_DIR"
+
+my_ip='10.253.0.22/24'
+my_name='saceph-mon2'
+net_brd='10.253.0.255'
+my_uuid="`uuidgen`"
+
+subs () {
+	local file="$1"
+	if [ ! -e "$file" ]; then
+		exit 1
+	fi
+	sed -i "$file" \
+		-e "s;@my_ip@;${my_ip};g" \
+		-e "s;@net_brd@;${net_brd};g" \
+		-e "s/@my_name@/${my_name}/g" \
+		-e "s/@my_uuid@/${my_uuid}/g"
+}
+
+subs openstack/latest/user_data
+subs openstack/latest/meta_data.json
--- rebuild-vm.sh
+++ rebuild-vm.sh
@@ -51,10 +51,36 @@ fi
 
 sudo chgrp adm "$vm_hdd"
 dd if=/dev/zero of="$vm_hdd" bs=1M count=32
-# export LIBGUESTFS_DEBUG=1
-# export LIBGUESTFS_TRACE=1
-# verbose="--verbose"
-virt-resize ${verbose} --expand /dev/sda1 "$UBUNTU_IMG" "$vm_hdd"
+
+provision_vm () {
+	local vm_hdd="$1"
+	local root_start=2048 # sectors
+	local drive_size=32 # GB
+	local swap_size=4 # GB
+	root_size=$((drive_size-swap_size))
+	root_end=$((root_start+root_size*1024*1024*2-1))
+
+	# export LIBGUESTFS_DEBUG=1
+	# export LIBGUESTFS_TRACE=1
+	local verbose=''
+	# verbose="--verbose"
+
+	guestfish ${verbose} <<-EOF
+		add "$UBUNTU_IMG"
+		add "$vm_hdd"
+		run
+		copy-device-to-device /dev/sda /dev/sdb size:$((512*root_start))
+		part-del /dev/sdb 1
+		part-add /dev/sdb p ${root_start} ${root_end}
+		part-add /dev/sdb p $((root_end+1)) -1
+		part-set-mbr-id /dev/sdb 2 0x82
+		mkswap-L MOREVM /dev/sdb2
+		copy-device-to-device /dev/sda1 /dev/sdb1
+		resize2fs /dev/sdb1
+	EOF
+}
+
+provision_vm $vm_hdd
 ./mkconfdrive "$vm"
 virsh start "$vm"
 

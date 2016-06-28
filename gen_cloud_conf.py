ecf57d904abec6eb2ca4d44dfbfe91bff1030a60
Alexey Sheplyakov <asheplyakov@mirantis.com>
2016-06-28 16:50:01 +0300

    gen_cloud_conf.py: move cloud-init template under templates/

--- gen_cloud_conf.py
+++ gen_cloud_conf.py
@@ -9,19 +9,17 @@ import sys
 import uuid
 
 MY_DIR = os.path.abspath(os.path.dirname(__file__))
+TEMPLATE_DIR = os.path.join(MY_DIR, 'templates/config-drive')
 
 
-def render_and_save(data, vm_name=None):
-    tmpl_dir = os.path.join(MY_DIR,
-                            'config-drive/template/openstack/latest')
+def render_and_save(data, vm_name=None, tmpl_dir=TEMPLATE_DIR):
     base_dir = '.build/config-drive/{0}'.format(vm_name)
     base_dir = os.path.join(MY_DIR, base_dir)
     out_dir = os.path.join(base_dir, 'openstack/latest')
     alt_dir = os.path.join(base_dir, 'openstack/2012-08-10')
-    if not os.path.isdir(out_dir):
-        os.makedirs(out_dir)
-    if not os.path.isdir(alt_dir):
-        os.makedirs(alt_dir)
+    for tdir in (out_dir, alt_dir):
+        if not os.path.isdir(tdir):
+            os.makedirs(tdir)
 
     env = jinja2.Environment(loader=jinja2.FileSystemLoader(tmpl_dir))
     for tmpl_name in ('user_data', 'meta_data.json'):
@@ -32,8 +30,7 @@ def render_and_save(data, vm_name=None):
             f.write(out)
         shutil.copy(out_file, alt_dir)
 
-    conf_img = os.path.join(MY_DIR, '{}-config.iso'.format(vm_name))
-    gen_iso(base_dir, conf_img)
+    return base_dir
 
 
 def get_authorized_keys(authorized_keys_file=None):
@@ -56,11 +53,13 @@ def gen_iso(src, dst):
     shutil.move('{}.tmp'.format(dst), dst)
 
 
-def generate_cc(data, vm_name=None):
+def generate_cc(data, vm_name=None, tmpl_dir=TEMPLATE_DIR):
     data['ssh_authorized_keys'] = get_authorized_keys()
     data['my_name'] = vm_name
     data['my_uuid'] = uuid.uuid4()
-    render_and_save(data, vm_name=vm_name)
+    out_dir = render_and_save(data, vm_name=vm_name, tmpl_dir=tmpl_dir)
+    conf_img = os.path.join(MY_DIR, '{}-config.iso'.format(vm_name))
+    gen_iso(out_dir, conf_img)
 
 
 def main():
@@ -72,6 +71,9 @@ def main():
     parser.add_option('--callback-port', dest='host_web_callback_port',
                       type=int, default=8080,
                       help='VM phone home URL')
+    parser.add_option('-t', '--template-dir', dest='tmpl_dir',
+                      default=TEMPLATE_DIR,
+                      help='config drive template dir')
     parser.add_option('--http-proxy', dest='http_proxy',
                       help='HTTP proxy for VMs')
     options, args = parser.parse_args()
@@ -83,7 +85,7 @@ def main():
         'host_web_callback_port': options.host_web_callback_port,
     }
     for vm_name in args:
-        generate_cc(data, vm_name=vm_name)
+        generate_cc(data, vm_name=vm_name, tmpl_dir=options.tmpl_dir)
 
 
 if __name__ == '__main__':
